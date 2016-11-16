d8ce8c6a186a897efbb0df2149865883f48f9f88
Shassan12 <Shahaan.hassan@gmail.com>
2016-11-16 17:51:55 +0000

    Added PuzzleGrid and Main method

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		int gridSize = 4;
+		PuzzleGrid grid = new PuzzleGrid(gridSize);
+	}
+
+}
--- src/PuzzleGrid.java
+++ src/PuzzleGrid.java
@@ -1,4 +1,60 @@
+import java.util.Scanner;
 
 public class PuzzleGrid {
-
+	private int gridSize;
+	private char[][] tileList;
+	private String gridStatus;
+	
+	public PuzzleGrid(int gridSize){
+		this.gridSize = gridSize;
+		this.tileList = new char[gridSize][gridSize];
+		initiateGrid();
+	}
+	
+	private void initiateGrid(){
+		Scanner scanner = new Scanner(System.in);
+		
+		while(true){
+			System.out.println("Input grid config as a string (e.g. aaaa for grid size 2):");
+			this.gridStatus = scanner.next();
+			
+			if(gridStatus.length() == gridSize*gridSize){
+				break;
+			}else{
+				System.out.println("invalid configuration. Configuration should have the same number of characters as the gridsize (" + gridSize+").");
+			}
+		}
+		
+		int pos = 0;
+		
+		for(int i=0; i<gridSize; i++){
+			for(int j=0; j<gridSize; j++){
+				tileList[i][j] = gridStatus.charAt(pos);
+				pos += 1;
+			}
+		}
+		
+		outputGrid();
+	}
+	
+	private boolean checkForGoal(String goal){
+		int pos = 0;
+		for(int i=0; i<gridSize; i++){
+			if(gridStatus.charAt(pos) != goal.charAt(pos)){
+				return false;
+			}
+		}
+		
+		return true;
+	}
+	
+	private void outputGrid(){
+		for(int i=0; i<gridSize; i++){
+			for(int j=0; j<gridSize; j++){
+				System.out.print(tileList[i][j]+" ");
+			}
+			
+			System.out.println();
+		}
+	}
 }
